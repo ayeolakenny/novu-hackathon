7bfd5ec710f550ac0ffc7b19c260f015bcf61d21
Bieefilled <65086865+PhetSirmhuel@users.noreply.github.com>
2023-05-07 13:59:45 +0100

    more changes

--- client/src/data/custom.jsx
+++ client/src/data/custom.jsx
@@ -4,23 +4,25 @@ export const Menu = [
   { name: "Listing", url: "/listing" },
   { name: "Contact", url: "/contact" },
 ];
-export const RentData = [{ name: "" }];
-export const Mapdata = [
+
+export const MapData = [
   {
-    title: "",
-    description: "",
-    position: "",
-    price: "",
+    title: "Full complex Flat",
+    description:
+      "loremLorem ipsum dolor sit amet, consectetur adipisicing elit. Labore quam necessitatibus vitae ex nam, similique officiis nesciunt sint, illo iure recusandae eaque optio illum facere voluptatibus hic animi. Aut, a!",
+    position: "Available",
+    price: "250",
     status: "",
     address: "",
     image:
       "https://images.pexels.com/photos/164522/pexels-photo-164522.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
   },
   {
-    title: "",
-    description: "",
-    position: "",
-    price: "",
+    title: "Self Contained ",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore quam necessitatibus vitae ex nam, similique officiis nesciunt sint, illo iure recusandae eaque optio illum facere voluptatibus hic animi. Aut, a!",
+    position: "Available",
+    price: "150",
     address: "",
     status: "",
 
@@ -28,14 +30,39 @@ export const Mapdata = [
       "https://images.pexels.com/photos/164558/pexels-photo-164558.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
   },
   {
-    title: "",
-    description: "",
-    position: "",
-    price: "",
+    title: "Mini Flat ",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore quam necessitatibus vitae ex nam, similique officiis nesciunt sint, illo iure recusandae eaque optio illum facere voluptatibus hic animi. Aut, a!",
+    position: "Available",
+    price: "300",
     address: "",
     status: "",
 
     image:
-      "https://images.pexels.com/photos/534151/pexels-photo-534151.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      "https://images.pexels.com/photos/323776/pexels-photo-323776.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+  {
+    title: "Self Contained ",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore quam necessitatibus vitae ex nam, similique officiis nesciunt sint, illo iure recusandae eaque optio illum facere voluptatibus hic animi. Aut, a!",
+    position: "Available",
+    price: "150",
+    address: "",
+    status: "",
+
+    image:
+      "https://images.pexels.com/photos/164558/pexels-photo-164558.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+  {
+    title: "Self Contained ",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore quam necessitatibus vitae ex nam, similique officiis nesciunt sint, illo iure recusandae eaque optio illum facere voluptatibus hic animi. Aut, a!",
+    position: "Available",
+    price: "150",
+    address: "",
+    status: "",
+
+    image:
+      "https://images.pexels.com/photos/164558/pexels-photo-164558.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
   },
 ];
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,13 +3,14 @@ import { Hero } from "../components/Hero";
 import { Map } from "../components/Map";
 import { FancyCard } from "../components/Card";
 export const Home = () => {
+  const location = () => {};
   return (
     <Fragment>
       <Hero />
-      <section className="relative h-screen z-20 lg:flex-col">
-        <Map />
-        <span className="absolute top-10 z-50 left-5 bg-white rounded-lg bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-0 shadow-lg w-4/12 ">
-          <FancyCard />
+      <section className="relative h-screen z-20 lg:flex-col ">
+        <Map location={location} />
+        <span className="absolute w-[500px] top-10 z-50 left-5 bg-blue-900 rounded-lg bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0 shadow-2xl  overflow-hidden">
+          <FancyCard location={location} />
         </span>
       </section>
     </Fragment>
